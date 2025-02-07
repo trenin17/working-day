20c1df9f2625eb157d422788a7142f7cf2867c60
Aleksandr Trenin <alex-trenin@mail.ru>
2025-02-07 00:43:25 +0000

    Add employee job position

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -189,3 +189,5 @@ CREATE TABLE IF NOT EXISTS working_day_first.employee_chats (
   FOREIGN KEY (employee_id) REFERENCES working_day_first.employees (id) ON DELETE CASCADE,
   FOREIGN KEY (chat_id) REFERENCES working_day_first.messenger_chats (chat_id) ON DELETE CASCADE
 );
+ALTER TABLE working_day_first.employees
+ADD COLUMN job_position TEXT;
