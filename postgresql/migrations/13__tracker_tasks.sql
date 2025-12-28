957a2795164c6497f9d9b1ce60fe9163f1687db3
Sofia Sabgir <sofiasabgir@yandex.by>
2025-12-28 06:47:06 +0000

    fix action_id

--- postgresql/migrations/13__tracker_tasks.sql
+++ postgresql/migrations/13__tracker_tasks.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS ${SCHEMA}.tracker_tasks (
     last_updated_ts TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     deadline TIMESTAMPTZ,
     action_id TEXT,
-    FOREIGN KEY (action_id) REFERENCES ${SCHEMA}.actions (action_id) ON DELETE CASCADE,
+    FOREIGN KEY (action_id) REFERENCES ${SCHEMA}.actions (id) ON DELETE CASCADE,
     FOREIGN KEY (project_id) REFERENCES ${SCHEMA}.tracker_projects (project_id) ON DELETE CASCADE,
     FOREIGN KEY (creator) REFERENCES ${SCHEMA}.employees (id) ON DELETE CASCADE,
     FOREIGN KEY (assignee) REFERENCES ${SCHEMA}.employees (id) ON DELETE CASCADE
